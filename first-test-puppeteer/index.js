9f912118d809eb6e77b37ef295f36b71f1fe1fe5
Guilherme <guilherme.teixeira013@protonmail.com>
2023-09-25 11:06:57 -0300

    ERROS solucionados

--- first-test-puppeteer/index.js
+++ first-test-puppeteer/index.js
@@ -3,10 +3,10 @@ const puppeteer = require('puppeteer');
 console.log("coins converter");
 
 (async () => {
-    const browser = await puppeteer.launch( headless, false );   // abrindo navegador
+    const browser = await puppeteer.launch({ headless: false});   // abrindo navegador
     const page = await browser.newPage();       // abrindo nova pagina
     await page.goto('https://google.com');      // preenchendo URL
     await page.screenshot({path: 'example.png'})// tirando print da página
     console.log("entrou");
     await browser.close();                      // fechando navegador
-})
+})();
